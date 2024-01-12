5de547aebb20afb8aeb2eb3bb26c53c4d0e2a0ff
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-01-12 18:43:51 +0530

    validation error

--- controllers/movie/validator.js
+++ controllers/movie/validator.js
@@ -4,7 +4,9 @@ exports.movieBody = [
   body("title").notEmpty().withMessage("Title is required"),
   body("year")
     .notEmpty()
-    .isInt({ min: 1800, max: new Date().getUTCFullYear() })
-    .withMessage(`Year can range in 1800 to ${new Date().getUTCFullYear()}`),
+    .isInt({ min: 1800, max: new Date().getUTCFullYear() + 25 })
+    .withMessage(
+      `Year can range in 1888 to ${new Date().getUTCFullYear() + 25} `
+    ),
   body("path").notEmpty().withMessage("Please upload the movie poster"),
 ];
